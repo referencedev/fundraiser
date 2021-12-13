3076bdb11a96b99759c926ac787efbcf445be928
Zavodil <support@zavodil.ru>
2021-12-14 00:42:38 +0200

    Audit suggestions https://github.com/referencedev/fundraiser/pull/7/commits/391c1960b1d94a523a4b3fd715b494718f4200c8, Unused links objects/methods removed

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,22 @@
-use near_sdk::{
-    AccountId, Balance, BorshStorageKey, env, ext_contract, Gas, near_bindgen, PanicOnDefault,
-    Promise, PromiseOrValue, PublicKey,
-};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::{LookupMap, UnorderedMap, UnorderedSet};
 use near_sdk::json_types::U128;
 use near_sdk::serde::{Deserialize, Serialize};
+use near_sdk::{
+    env, ext_contract, near_bindgen, AccountId, Balance, BorshStorageKey, Gas, PanicOnDefault,
+    Promise, PromiseOrValue, PublicKey,
+};
 
 use crate::sale::VSale;
 
-mod sale;
-mod token_receiver;
 mod migration_0;
 mod migration_1;
-
-pub(crate) const ONE_NEAR: Balance = 10u128.pow(24);
-
-const BASE_GAS: Gas = Gas(5_000_000_000_000);
-pub(crate) const CREATE_ACCOUNT_GAS: Gas = Gas(4 * BASE_GAS.0);
-pub(crate) const ON_CREATE_ACCOUNT_GAS: Gas = Gas(4 * BASE_GAS.0);
+mod sale;
+mod token_receiver;
 
 const NO_DEPOSIT: Balance = 0;
-const ACCESS_KEY_ALLOWANCE: Balance = ONE_NEAR / 100;
-// AUDIT: This should be more than `ACCESS_KEY_ALLOWANCE` to cover cost of storage for access key + allowance.
-const CREATE_LINK_AMOUNT: Balance = ONE_NEAR / 100;
-const CREATE_ACCOUNT_AMOUNT: Balance = ONE_NEAR / 100;
-
 const REFERRAL_FEE_DENOMINATOR: u128 = 10000;
-const NEAR_ACCOUNT: &str = "near";
 const WRAP_NEAR_ACCOUNT: &str = "wrap.near";
-
 
 #[ext_contract(ext_self)]
 pub trait ExtContract {
@@ -53,19 +40,28 @@ pub trait ExtContract {
     ) -> PromiseOrValue<U128>;
 
     /// Callback after token claim
-    fn after_withdraw_purchase(&mut self,
-                               account_id: AccountId,
-                               amount_to_claim: U128,
-                               sale_id: u64) -> bool;
+    fn after_withdraw_purchase(
+        &mut self,
+        account_id: AccountId,
+        amount_to_claim: U128,
+        sale_id: u64,
+    ) -> bool;
 
     /// Callback after token refund for subscription sales
-    fn after_refund_purchase(&mut self,
-                             account_id: AccountId,
-                             amount_to_refund: U128,
-                             sale_id: u64) -> bool;
+    fn after_refund_purchase(
+        &mut self,
+        account_id: AccountId,
+        amount_to_refund: U128,
+        sale_id: u64,
+    ) -> bool;
 
     /// Callback after affiliate_rewards claim
-    fn after_withdraw_affiliate_reward(&mut self, account_id: AccountId, amount: U128, sale_id: u64) -> bool;
+    fn after_withdraw_affiliate_reward(
+        &mut self,
+        account_id: AccountId,
+        amount: U128,
+        sale_id: u64,
+    ) -> bool;
 }
 
 #[derive(BorshSerialize, BorshDeserialize)]
@@ -77,7 +73,6 @@ pub struct AccountOld {
 #[derive(BorshSerialize, BorshDeserialize)]
 pub struct Account {
     referrer: AccountId,
-    links: UnorderedSet<PublicKey>,
     affiliates: LookupMap<u8, UnorderedSet<AccountId>>,
 }
 
@@ -104,8 +99,16 @@ struct AccountOutput {
 impl From<Account> for AccountOutput {
     fn from(account: Account) -> Self {
         let aff_0 = get_affiliates_num(&account, 0);
-        let aff_1 = if aff_0 > 0 { get_affiliates_num(&account, 1) } else { 0 };
-        let aff_2 = if aff_1 > 0 { get_affiliates_num(&account, 2) } else { 0 };
+        let aff_1 = if aff_0 > 0 {
+            get_affiliates_num(&account, 1)
+        } else {
+            0
+        };
+        let aff_2 = if aff_1 > 0 {
+            get_affiliates_num(&account, 2)
+        } else {
+            0
+        };
 
         Self {
             referrer: account.referrer,
@@ -126,9 +129,6 @@ impl Account {
     pub fn new(account_id: &AccountId, referrer: &AccountId) -> Self {
         Self {
             referrer: referrer.clone(),
-            links: UnorderedSet::new(StorageKey::AccountLinks {
-                account_id: account_id.clone(),
-            }),
             affiliates: LookupMap::new(StorageKey::Affiliates {
                 account_id: account_id.clone(),
             }),
@@ -136,13 +136,14 @@ impl Account {
     }
 }
 
+#[allow(dead_code)]
 #[derive(BorshStorageKey, BorshSerialize)]
 pub(crate) enum StorageKey {
     Accounts,
     Sales,
     AccountSales { sale_id: u64 },
     Links,
-    AccountLinks { account_id: AccountId },
+    AccountLinks,
     AccountAffiliateRewards { sale_id: u64 },
     Affiliates { account_id: AccountId },
     AffiliateLevels { account_id: AccountId, level: u8 },
@@ -158,20 +159,9 @@ struct Contract {
     referral_fees: Vec<u64>,
     accounts: UnorderedMap<AccountId, VAccount>,
     sales: LookupMap<u64, VSale>,
-    links: LookupMap<PublicKey, AccountId>,
     num_sales: u64,
     // not used anymore
     accounts_old: UnorderedMap<AccountId, AccountOld>,
-}
-
-impl Contract {
-    fn internal_remove_link(&mut self, account_id: AccountId, public_key: PublicKey) -> Promise {
-        let mut account: Account = self.accounts.get(&account_id).expect("ERR_NO_ACCOUNT").into();
-        self.links.remove(&public_key);
-        account.links.remove(&public_key);
-        self.accounts.insert(&account_id, &VAccount::Current(account));
-        Promise::new(env::current_account_id()).delete_key(public_key)
-    }
 }
 
 #[near_bindgen]
@@ -184,7 +174,6 @@ impl Contract {
             referral_fees,
             accounts: UnorderedMap::new(StorageKey::Accounts),
             sales: LookupMap::new(StorageKey::Sales),
-            links: LookupMap::new(StorageKey::Links),
             num_sales: 0,
             accounts_old: UnorderedMap::new(StorageKey::AccountsV1),
         };
@@ -192,103 +181,32 @@ impl Contract {
             &this.owner_id,
             &VAccount::Current(Account::new(&this.owner_id, &this.owner_id)),
         );
-        // AUDIT: If this contract has other access keys (e.g. full access key), then it should
-        // be whitelisted by the owner. Otherwise an attacker may use `create_link` to claim this
-        // public key and then delete it, which can be used to remove any public key from this
-        // account that was not whitelisted before.
         this
-    }
-
-    #[payable]
-    pub fn create_link(&mut self, public_key: PublicKey) -> Promise {
-        assert_eq!(env::attached_deposit(), CREATE_LINK_AMOUNT);
-        let mut account: Account = self
-            .accounts
-            .get(&env::predecessor_account_id())
-            .expect("ERR_NO_ACCOUNT")
-            .into();
-        assert!(self.links.get(&public_key).is_none(), "ERR_DUPLICATE_KEY");
-        self.links
-            .insert(&public_key, &env::predecessor_account_id());
-        account.links.insert(&public_key);
-        self.accounts
-            .insert(&env::predecessor_account_id(), &VAccount::Current(account));
-        Promise::new(env::current_account_id()).add_access_key(
-            public_key,
-            ACCESS_KEY_ALLOWANCE,
-            env::current_account_id(),
-            "create_account".to_string(),
-        )
-    }
-
-    pub fn remove_link(&mut self, public_key: PublicKey) -> Promise {
-        let account_id = self.links.get(&public_key).expect("ERR_NO_LINK");
-        assert_eq!(
-            account_id,
-            env::predecessor_account_id(),
-            "ERR_NOT_LINK_OWNER"
-        );
-        self.internal_remove_link(env::predecessor_account_id(), public_key)
-    }
-
-    /// Only can be called by the access key on this contract.
-    /// Can be added via create_link.
-    pub fn create_account(&mut self, account_id: AccountId, public_key: PublicKey) -> Promise {
-        assert_eq!(env::predecessor_account_id(), env::current_account_id());
-        Promise::new(AccountId::new_unchecked(NEAR_ACCOUNT.to_string()))
-            .function_call(
-                "create_account".to_string(),
-                format!(
-                    "{{\"new_account_id\": \"{}\", \"new_public_key\": \"{:?}\"}}",
-                    account_id, public_key
-                )
-                .into_bytes(),
-                CREATE_ACCOUNT_AMOUNT,
-                CREATE_ACCOUNT_GAS,
-            )
-            .then(ext_self::on_create_account(
-                account_id,
-                env::current_account_id(),
-                NO_DEPOSIT,
-                ON_CREATE_ACCOUNT_GAS,
-            ))
-    }
-
-    /// Callback after account was created by near.
-    /// Add an internal account with referrer and remove the link info.
-    pub fn on_create_account(&mut self, new_account_id: AccountId) -> Promise {
-        assert_eq!(env::predecessor_account_id(), env::current_account_id());
-        let referrer = self
-            .links
-            .get(&env::signer_account_pk())
-            .expect("ERR_NO_LINK");
-        self.accounts
-            .insert(&new_account_id, &VAccount::Current(Account::new(&new_account_id, &referrer)));
-        // AUDIT: Predecessor here is `env::current_account_id()`, so it's a bug. I guess you can
-        // use `referrer` here instead.
-        // BEFORE AUDIT: self.internal_remove_link(env::predecessor_account_id(), env::signer_account_pk())
-        self.internal_remove_link(referrer, env::signer_account_pk())
     }
 
     #[payable]
     pub fn join(&mut self, referrer_id: Option<AccountId>) {
         let account_id = env::predecessor_account_id();
-        let referrer_id_unwrapped: AccountId =
-            if let Some(referrer_id_unwrapped) = referrer_id {
-                if self.accounts.get(&referrer_id_unwrapped).is_some() {
-                    referrer_id_unwrapped
-                } else {
-                    self.owner_id.clone()
-                }
+        let referrer_id_unwrapped: AccountId = if let Some(referrer_id_unwrapped) = referrer_id {
+            if self.accounts.get(&referrer_id_unwrapped).is_some() {
+                referrer_id_unwrapped
             } else {
                 self.owner_id.clone()
-            };
+            }
+        } else {
+            self.owner_id.clone()
+        };
 
         assert_ne!(referrer_id_unwrapped, account_id, "SELF_REFERRER");
-        assert!(self.accounts.get(&account_id).is_none(), "ERR_ACCOUNT_EXISTS");
+        assert!(
+            self.accounts.get(&account_id).is_none(),
+            "ERR_ACCOUNT_EXISTS"
+        );
         assert_eq!(env::attached_deposit(), self.join_fee, "ERR_FEE");
-        self.accounts
-            .insert(&account_id, &VAccount::Current(Account::new(&account_id, &referrer_id_unwrapped)));
+        self.accounts.insert(
+            &account_id,
+            &VAccount::Current(Account::new(&account_id, &referrer_id_unwrapped)),
+        );
 
         // Don't save internal affiliates to save storage and gas
         if self.owner_id != referrer_id_unwrapped {
@@ -296,21 +214,30 @@ impl Contract {
         }
     }
 
-    fn insert_affiliate_on_level(&mut self, referrer_id: &AccountId, mut referrer_account: Account, level: u8, affiliate_account_id: &AccountId) {
+    fn insert_affiliate_on_level(
+        &mut self,
+        referrer_id: &AccountId,
+        mut referrer_account: Account,
+        level: u8,
+        affiliate_account_id: &AccountId,
+    ) {
         let mut affiliates_on_level: UnorderedSet<AccountId> =
             if let Some(affiliates_unwrapped) = referrer_account.affiliates.get(&level) {
                 affiliates_unwrapped
             } else {
                 UnorderedSet::new(StorageKey::AffiliateLevels {
-                    account_id: affiliate_account_id.clone(),
+                    account_id: referrer_id.clone(),
                     level,
                 })
             };
 
         affiliates_on_level.insert(affiliate_account_id);
-        referrer_account.affiliates.insert(&level, &affiliates_on_level);
+        referrer_account
+            .affiliates
+            .insert(&level, &affiliates_on_level);
 
-        self.accounts.insert(referrer_id, &VAccount::Current(referrer_account));
+        self.accounts
+            .insert(referrer_id, &VAccount::Current(referrer_account));
     }
 
     fn insert_affiliates(&mut self, referrer_1_id: AccountId, account_id: AccountId) {
@@ -319,14 +246,28 @@ impl Contract {
             let referrer_2_id = &referrer_account_1.referrer.clone();
             self.insert_affiliate_on_level(&referrer_1_id, referrer_account_1, 0, &account_id);
 
-            if let Some(referrer_v_account_2) = self.accounts.get(referrer_2_id) {
-                let referrer_account_2: Account = referrer_v_account_2.into();
-                let referrer_3_id = &referrer_account_2.referrer.clone();
-                self.insert_affiliate_on_level(referrer_2_id, referrer_account_2, 1, &account_id);
+            if self.owner_id != referrer_2_id {
+                if let Some(referrer_v_account_2) = self.accounts.get(referrer_2_id) {
+                    let referrer_account_2: Account = referrer_v_account_2.into();
+                    let referrer_3_id = &referrer_account_2.referrer.clone();
+                    self.insert_affiliate_on_level(
+                        referrer_2_id,
+                        referrer_account_2,
+                        1,
+                        &account_id,
+                    );
 
-                if let Some(referrer_v_account_3) = self.accounts.get(referrer_3_id) {
-                    let referrer_account_3: Account = referrer_v_account_3.into();
-                    self.insert_affiliate_on_level(referrer_3_id, referrer_account_3, 2, &account_id);
+                    if self.owner_id != referrer_3_id {
+                        if let Some(referrer_v_account_3) = self.accounts.get(referrer_3_id) {
+                            let referrer_account_3: Account = referrer_v_account_3.into();
+                            self.insert_affiliate_on_level(
+                                referrer_3_id,
+                                referrer_account_3,
+                                2,
+                                &account_id,
+                            );
+                        }
+                    }
                 }
             }
         }
@@ -341,7 +282,8 @@ impl Contract {
     }
 
     pub fn get_account(&self, account_id: AccountId) -> AccountOutput {
-        let account: Account = self.accounts
+        let account: Account = self
+            .accounts
             .get(&account_id)
             .expect("ERR_ACCOUNT_DOESNT_EXIST")
             .into();
@@ -363,15 +305,6 @@ impl Contract {
             })
             .collect()
     }
-
-    pub fn get_link_referrer(&self, public_key: PublicKey) -> AccountId {
-        self.links.get(&public_key).expect("ERR_NO_KEY")
-    }
-
-    pub fn get_links(&self, account_id: AccountId) -> Vec<PublicKey> {
-        let account: Account = self.accounts.get(&account_id).expect("ERR_NO_ACCOUNT").into();
-        account.links.to_vec()
-    }
 }
 
 #[cfg(test)]
@@ -379,10 +312,10 @@ mod tests {
     use std::str::FromStr;
 
     use near_contract_standards::fungible_token::receiver::FungibleTokenReceiver;
-    use near_sdk::{PromiseResult, serde_json, testing_env};
     use near_sdk::json_types::U64;
-    use near_sdk::test_utils::{accounts, testing_env_with_promise_results};
     use near_sdk::test_utils::VMContextBuilder;
+    use near_sdk::test_utils::{accounts, testing_env_with_promise_results};
+    use near_sdk::{serde_json, testing_env, PromiseResult};
 
     use crate::sale::{SaleInput, SaleMetadata, SaleType};
     use crate::token_receiver::SaleDeposit;
@@ -411,7 +344,7 @@ mod tests {
                 project_medium: None,
                 project_twitter: None,
                 reward_timestamp: None,
-                reward_description: None
+                reward_description: None,
             },
             staking_contracts: vec![AccountId::new_unchecked("test.staking".to_string())],
             min_near_deposit: U128(100),
@@ -429,7 +362,7 @@ mod tests {
             price: U128(1000),
             whitelist_hash: None,
             limit_per_transaction: U128(100),
-            sale_type: SaleType::ByAmount
+            sale_type: SaleType::ByAmount,
         });
         assert_eq!(contract.get_referral_fees(), referral_fees);
         assert_eq!(contract.get_join_fee(), join_fee);
--- src/migration_0.rs
+++ src/migration_0.rs
@@ -1,7 +1,7 @@
 use near_sdk::log;
 
-use crate::*;
 use crate::sale::*;
+use crate::*;
 
 #[near_bindgen]
 impl Contract {
@@ -29,34 +29,31 @@ impl Contract {
             referral_fees: old_contract.referral_fees,
             accounts: UnorderedMap::new(StorageKey::AccountsV1),
             sales: old_contract.sales,
-            links: old_contract.links,
             num_sales: old_contract.num_sales,
             accounts_old: old_contract.accounts,
         }
     }
 
     #[private]
-    pub fn migrate_a1(&mut self, limit: u64) { // accounts_old transition
+    pub fn migrate_a1(&mut self, limit: u64) {
+        // accounts_old transition
         let keys = self.accounts_old.keys_as_vector();
-        let account_ids: Vec<AccountId> =
-            (0..std::cmp::min(limit,  keys.len()))
-                .map(|index| keys.get(index).unwrap().into())
-                .collect();
+        let account_ids: Vec<AccountId> = (0..std::cmp::min(limit, keys.len()))
+            .map(|index| keys.get(index).unwrap().into())
+            .collect();
 
         for account_id in account_ids {
-            let account_old: AccountOld = self.accounts_old.get(&account_id).unwrap();
+            let account_old: AccountOld = self.accounts_old.remove(&account_id).unwrap();
             let account = Account {
                 referrer: account_old.referrer,
-                links: account_old.links,
                 affiliates: LookupMap::new(StorageKey::Affiliates {
                     account_id: account_id.clone(),
                 }),
             };
-            self.accounts.insert(&account_id, &VAccount::Current(account));
-            self.accounts_old.remove(&account_id);
+            self.accounts
+                .insert(&account_id, &VAccount::Current(account));
         }
 
         log!("Pending items: {}", self.accounts_old.len());
     }
-
 }
